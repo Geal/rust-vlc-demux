baee342ae72cbf2dbe31094373482babe11ad4e7
Geoffroy Couprie <geo.couprie@gmail.com>
2016-05-14 11:35:32 +0200

    manually generate the module metadata

--- src/lib.rs
+++ src/lib.rs
@@ -20,10 +20,108 @@ use libc::{size_t,c_int,c_void};
 
 //type vlc_set_cb = FnOnce(*mut c_void, *mut c_void, c_int, ...) -> c_int;
 
-#[no_mangle]
-pub extern fn vlc_entry__3_0_0a(vlc_set: unsafe extern fn(*mut c_void, *mut c_void, *mut c_void, c_int, ...) -> c_int,
-  opaque: *mut c_void) -> c_int {
+#[repr(i32)]
+enum VLCModuleProperties {
+    VLC_MODULE_CREATE = 0,
+    VLC_CONFIG_CREATE,
 
- -1
+    VLC_MODULE_CPU_REQUIREMENT=0x100,
+    VLC_MODULE_SHORTCUT,
+    VLC_MODULE_CAPABILITY,
+    VLC_MODULE_SCORE,
+    VLC_MODULE_CB_OPEN,
+    VLC_MODULE_CB_CLOSE,
+    VLC_MODULE_NO_UNLOAD,
+    VLC_MODULE_NAME,
+    VLC_MODULE_SHORTNAME,
+    VLC_MODULE_DESCRIPTION,
+    VLC_MODULE_HELP,
+    VLC_MODULE_TEXTDOMAIN,
+
+    VLC_CONFIG_NAME=0x1000,
+    VLC_CONFIG_VALUE,
+    VLC_CONFIG_RANGE,
+    VLC_CONFIG_ADVANCED,
+    VLC_CONFIG_VOLATILE,
+    VLC_CONFIG_PERSISTENT_OBSOLETE,
+    VLC_CONFIG_PRIVATE,
+    VLC_CONFIG_REMOVED,
+    VLC_CONFIG_CAPABILITY,
+    VLC_CONFIG_SHORTCUT,
+    VLC_CONFIG_OLDNAME_OBSOLETE,
+    VLC_CONFIG_SAFE,
+    VLC_CONFIG_DESC,
+    VLC_CONFIG_LIST_OBSOLETE,
+    VLC_CONFIG_ADD_ACTION_OBSOLETE,
+    VLC_CONFIG_LIST,
+    VLC_CONFIG_LIST_CB,
 }
 
+#[no_mangle]
+pub extern fn vlc_entry__3_0_0a(vlc_set: unsafe extern fn(*mut c_void, *mut c_void, c_int, ...) -> c_int,
+  opaque: *mut c_void) -> c_int {
+  let module: *mut c_void = 0 as *mut c_void;
+  let config: *mut c_void = 0 as *mut c_void;
+
+  unsafe {
+    if vlc_set(opaque, 0 as *mut c_void, VLCModuleProperties::VLC_MODULE_CREATE as i32, &module) != 0 {
+      panic!("cannot create module");
+      return -1;
+    }
+  }
+
+  let name = b"inrustwetrust\0";
+  unsafe {
+    if vlc_set(opaque, module, VLCModuleProperties::VLC_MODULE_NAME as i32, name.as_ptr()) != 0 {
+      panic!("cannot set module name");
+      return -1;
+    }
+  }
+
+  let desc = b"FLV demuxer written in Rust\0";
+  unsafe {
+    if vlc_set(opaque, module, VLCModuleProperties::VLC_MODULE_DESCRIPTION as i32, desc.as_ptr()) != 0 {
+      panic!("cannot set module description");
+      return -1;
+    }
+  }
+
+  let capability = b"demux\0";
+  unsafe {
+    if vlc_set(opaque, module, VLCModuleProperties::VLC_MODULE_CAPABILITY as i32, capability.as_ptr()) != 0 {
+      panic!("cannot set module capability");
+      return -1;
+    }
+  }
+
+  unsafe {
+    if vlc_set(opaque, module, VLCModuleProperties::VLC_MODULE_SCORE as i32, 999) != 0 {
+      panic!("cannot set module score");
+      return -1;
+    }
+  }
+
+  unsafe {
+    if vlc_set(opaque, module, VLCModuleProperties::VLC_MODULE_CB_OPEN as i32, open) != 0 {
+      panic!("cannot set module open callback");
+      return -1;
+    }
+  }
+
+  unsafe {
+    if vlc_set(opaque, module, VLCModuleProperties::VLC_MODULE_CB_CLOSE as i32, close) != 0 {
+      panic!("cannot set module close callback");
+      return -1;
+    }
+  }
+  //panic!("POUETPOUETPOUET");
+ 0
+}
+
+fn open(obj: *mut c_void) -> c_int {
+  panic!("IN OPEN");
+}
+
+fn close(obj: *mut c_void) {
+  panic!("IN CLOSE");
+}
