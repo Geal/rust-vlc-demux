3dcf92a33e9434c9c58fd0eb3d71a7890df7eadf
Geoffroy Couprie <geo.couprie@gmail.com>
2016-05-14 14:58:42 +0200

    extract the module properties enum in another file

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,12 @@
 #![feature(start)]
 #![no_std]
 
+mod vlc;
+
 extern crate libc;
 use libc::{size_t,c_int,c_void};
+
+use vlc::VLCModuleProperties;
 
 //static int Open ( vlc_object_t * );
 //static void Close( vlc_object_t * );
@@ -19,43 +23,6 @@ use libc::{size_t,c_int,c_void};
 //EXTERN_SYMBOL typedef int (*vlc_set_cb) (void *, void *, int, ...);
 
 //type vlc_set_cb = FnOnce(*mut c_void, *mut c_void, c_int, ...) -> c_int;
-
-#[repr(i32)]
-enum VLCModuleProperties {
-    VLC_MODULE_CREATE = 0,
-    VLC_CONFIG_CREATE,
-
-    VLC_MODULE_CPU_REQUIREMENT=0x100,
-    VLC_MODULE_SHORTCUT,
-    VLC_MODULE_CAPABILITY,
-    VLC_MODULE_SCORE,
-    VLC_MODULE_CB_OPEN,
-    VLC_MODULE_CB_CLOSE,
-    VLC_MODULE_NO_UNLOAD,
-    VLC_MODULE_NAME,
-    VLC_MODULE_SHORTNAME,
-    VLC_MODULE_DESCRIPTION,
-    VLC_MODULE_HELP,
-    VLC_MODULE_TEXTDOMAIN,
-
-    VLC_CONFIG_NAME=0x1000,
-    VLC_CONFIG_VALUE,
-    VLC_CONFIG_RANGE,
-    VLC_CONFIG_ADVANCED,
-    VLC_CONFIG_VOLATILE,
-    VLC_CONFIG_PERSISTENT_OBSOLETE,
-    VLC_CONFIG_PRIVATE,
-    VLC_CONFIG_REMOVED,
-    VLC_CONFIG_CAPABILITY,
-    VLC_CONFIG_SHORTCUT,
-    VLC_CONFIG_OLDNAME_OBSOLETE,
-    VLC_CONFIG_SAFE,
-    VLC_CONFIG_DESC,
-    VLC_CONFIG_LIST_OBSOLETE,
-    VLC_CONFIG_ADD_ACTION_OBSOLETE,
-    VLC_CONFIG_LIST,
-    VLC_CONFIG_LIST_CB,
-}
 
 #[no_mangle]
 pub extern fn vlc_entry__3_0_0a(vlc_set: unsafe extern fn(*mut c_void, *mut c_void, c_int, ...) -> c_int,
