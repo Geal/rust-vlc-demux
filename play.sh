ee5b6381038c395f1fc5403d32e3bef1551d38e3
Geoffroy Couprie <geo.couprie@gmail.com>
2016-05-18 18:27:01 +0200

    we can test with audio now

--- play.sh
+++ play.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
- ./VLC.app/Contents/MacOS/VLC -vvv --reset-plugins-cache --no-audio ./zeldaHQ.flv vlc://quit 2>&1
+ ./VLC.app/Contents/MacOS/VLC -vvv --reset-plugins-cache ./zeldaHQ.flv vlc://quit 2>&1
